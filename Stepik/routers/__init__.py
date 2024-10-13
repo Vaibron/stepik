8e32a2191f5c2885be7edab62f3b7c54821b89d2
mr.vaibron <160307500+Vaibron@users.noreply.github.com>
2024-10-13 21:25:31 +0300

    Delete Stepik directory

--- Stepik/routers/__init__.py
+++
@@ -1,5 +0,0 @@
-from routers.admin_router import admin_router
-from routers.api_router import api_router
-from routers.auth_router import auth_router
-from routers.course_router import course_router
-from routers.user_router import user_router
--- Stepik/routers/api_router.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-api_router = APIRouter()
-
-
-@api_router.get("/")
-def index():
-    return {"message": "Welcome to Stepik API!"}
-
